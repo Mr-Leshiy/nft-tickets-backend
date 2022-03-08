99fd5da306e6cf516d98daa73707f773abe7c60f
Mr-Leshiy <leshiy12345678@gmail.com>
2022-03-08 16:40:12 +0200

    add initial server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer};
+
+async fn hello_world(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
+
+pub fn config_app(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/").route(web::get().to(hello_world)));
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server");
+
+    HttpServer::new(|| App::new().configure(config_app))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
