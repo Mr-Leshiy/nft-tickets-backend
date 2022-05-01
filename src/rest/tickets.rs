113222f18aa2f701fa311d6902e5c853fdf57b1a
Mr-Leshiy <leshiy12345678@gmail.com>
2022-05-01 21:33:46 +0300

    add tests

--- src/rest/tickets.rs
+++ src/rest/tickets.rs
@@ -19,4 +19,31 @@ pub fn tickets() -> Scope {
 }
 
 #[cfg(test)]
-mod test {}
+mod tests {
+    use crate::rest::config_app;
+    use actix_web::{test, App};
+
+    #[tokio::test]
+    async fn create_collection_test() {
+        let app = test::init_service(App::new().configure(config_app)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/tickets/collection")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+    }
+
+    #[tokio::test]
+    async fn get_collection_test() {
+        let app = test::init_service(App::new().configure(config_app)).await;
+
+        let req = test::TestRequest::get()
+            .uri(format!("/tickets/collection/{}", "id1").as_str())
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+    }
+}
